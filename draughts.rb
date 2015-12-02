dcbf62574b755a6078858ff63141473dbd508283
Rafael Gibim <gibim6@gmail.com>
2015-12-02 11:37:11 +0000

    todo: implement a legal_move? for only one move, but check a bunch

--- draughts.rb
+++ draughts.rb
@@ -2,21 +2,21 @@ require_relative './board.rb'
 require 'scanf'
 
 class Draughts
-  $white = 0
+  $white = 0 ## lower side
   $black = 1
   def initialize
     @positions = Matrix[
-        [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-         [' ',' ',' ',' ',' ','o','o','o','o',' '],
-          [' ',' ',' ',' ','o','o',' ', 'o','o',' '],
-           [' ',' ',' ', 'o',' ', 'o','o',' ', 'o',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-             [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-              [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-               [' ','x',' ','x','x',' ','x',' ',' ',' '],
-                [' ','x','x',' ','x','x',' ',' ',' ',' '],
-                 [' ','x','x','x','x',' ',' ',' ',' ',' '],
-                  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        ['E','E','E','E','E','E','E','E','E','E'],
+         ['E','E','E','E','E','o','o','o','o','E'],
+          ['E','E','E','E','o','o',' ','o','o','E'],
+           ['E','E','E','o',' ','o','o',' ','o','E'],
+            ['E','E',' ',' ',' ',' ',' ',' ',' ','E'],
+             ['E',' ',' ',' ',' ',' ',' ',' ',' ','E'],
+              ['E',' ',' ',' ',' ',' ',' ',' ','E','E'],
+               ['E','x',' ','x','x',' ','x','E','E','E'],
+                ['E','x','x',' ','x','x','E','E','E','E'],
+                 ['E','x','x','x','x','E','E','E','E','E'],
+                  ['E','E','E','E','E','E','E','E','E','E']
     ]
     @board_pairs = Board.prepare_pairs
     @black_pieces_left = 12
@@ -35,7 +35,7 @@ class Draughts
     puts "It's black's turn" if color == $black
     loop do
       puts "\nPlease input your move below (eg \'d2 ne\' or \'c5 h4 nw\')"
-      str = scanf("%s %s")
+      str = gets.chomp.split
       if legal_move?(str) then
         break
       else
@@ -45,40 +45,55 @@ class Draughts
   end
 
   def legal_move? str
-    piece = str[0]
-    direction = str[1]
-    return false unless @board_pairs.key(piece) # check if the cell exist
-    piece = @board_pairs.key(piece).scanf("%d %d") # cell is into "row column" (for the matrix)
-    x = piece[0]
-    y = piece[1]
-    return false unless['x','X','o','O'].include?(@positions[x, y])
-    return false unless['nw', 'ne', 'sw', 'se'].include?(direction) # check if direction is valid
+    piece = str.shift; directions = str
+    return false if not @board_pairs.key(piece) # check if the cell exist
+    
+    piece = @board_pairs.key(piece).scanf("%d %d") # cell into "row column" (for the matrix)
+    x = piece[0]; y = piece[1] # just to simplify future uses of those variables
 
-    if direction == 'nw'
-      if @positions[x-1,y] == ' '
-        return true
-      end
+    arr = ['x','X'] if @whose_turn == $white
+    arr = ['o','O'] if @whose_turn == $black
+
+    return false if not arr.include?(@positions[x, y]) # check if there's valid piece on the cell 
+    return false if not ((str - ['nw', 'ne', 'sw', 'se']).empty?) # check if direction is valid
+
+    str.each do |direction|
+      return false if not destination_in_bounds?(x,y,direction)
+      return false if destination_same_color?(x,y,direction)
     end
+    return true
+  end
 
-    if direction == 'ne'
-      if @positions[x-1,y+1] == ' '
-        return true
-      end
+  def destination_in_bounds? x, y, dest
+    case dest
+    when 'nw' then
+      return true unless @positions[x-1,y] == 'E'
+    when 'ne' then
+      return true unless @positions[x-1,y+1] == 'E'
+    when 'sw' then
+      return true unless @positions[x+1,y-1] == 'E'
+    when 'se' then
+      return true unless @positions[x+1,y] == 'E'
+    else
+      return false
     end
+  end
 
-    if direction == 'sw'
-      if @positions[x+1,y-1] == ' '
-        return true
-      end
+  def destination_same_color? x, y, dest
+    arr = ['x','X'] if @whose_turn == $white
+    arr = ['o','O'] if @whose_turn == $black
+    case dest
+    when 'nw' then
+      return true if arr.include?@positions[x-1,y]
+    when 'ne' then
+      return true if arr.include?@positions[x-1,y+1]
+    when 'sw' then
+      return true if arr.include?@positions[x+1,y-1]
+    when 'se' then
+      return true if arr.include?@positions[x+1,y]
+    else
+      return false
     end
-
-    if direction == 'se'
-      if @positions[x+1,y] == ' '
-        return true
-      end
-    end
-
-    return false
   end
 
   def print_board
