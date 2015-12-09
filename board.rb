07fb9e2debea835bf1667426a468f3348d3ef90f
Rafael Gibim <gibim6@gmail.com>
2015-12-09 00:18:28 +0000

    Rubocop fixes, code tweaks...

--- board.rb
+++ board.rb
@@ -9,40 +9,40 @@ class Board
     'se' => [+1, 0]
   }
   CELL_CODES = Matrix[
-    [0,0,0,0,0,1,3,3,3,2],
-     [0,0,0,0,5,7,7,7,7,2],
-      [0,0,0,5,7,7,7,7,7,2],
-       [0,0,5,7,7,7,7,7,7,2],
-        [0,5,7,7,7,7,7,7,7,2],
-         [4,7,7,7,7,7,7,7,7,0],
-          [4,7,7,7,7,7,7,7,0,0],
-           [4,7,7,7,7,7,7,0,0,0],
-            [4,7,7,7,7,7,0,0,0,0],
-             [4,7,7,7,7,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0]
+    [0, 0, 0, 0, 0, 1, 3, 3, 3, 2],
+    [0, 0, 0, 0, 5, 7, 7, 7, 7, 2],
+    [0, 0, 0, 5, 7, 7, 7, 7, 7, 2],
+    [0, 0, 5, 7, 7, 7, 7, 7, 7, 2],
+    [0, 5, 7, 7, 7, 7, 7, 7, 7, 2],
+    [4, 7, 7, 7, 7, 7, 7, 7, 7, 0],
+    [4, 7, 7, 7, 7, 7, 7, 7, 0, 0],
+    [4, 7, 7, 7, 7, 7, 7, 0, 0, 0],
+    [4, 7, 7, 7, 7, 7, 0, 0, 0, 0],
+    [4, 7, 7, 7, 7, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]
 
-  def self.prepare_board positions
-    seq = [' ',1,2,3,4,5,6,7,8,9,' ']
+  def self.prepare_board(positions)
+    seq = [' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, ' ']
     counter = 0
     indentation = 0
     ret = ''
 
     CELL_CODES.to_a.map.with_index do |m, i|
-      if i != 10 then
-        ret += seq[counter].to_s + ' ' * indentation 
-        m.each_with_index do |n, j| 
-          n == 7 ? ret += ("#{positions[i,j]} ") : ret += ('  ')  # piece occupying the cell
-          [4,5,6,7].include?(n) ? ret += ('| ') : ret += ('  ') 
+      if i != 10
+        ret += seq[counter].to_s + ' ' * indentation
+        m.each_with_index do |n, j|
+          n == 7 ? ret += ("#{positions[i, j]} ") : ret += ('  ') # piece occupying the cell
+          [4, 5, 6, 7].include?(n) ? ret += ('| ') : ret += ('  ')
         end
-        ret += "\n" + ' ' * indentation unless i == CELL_CODES.to_a.map.size-1
+        ret += "\n" + ' ' * indentation unless i == CELL_CODES.to_a.map.size - 1
         m.each_with_index do |n|
-          [2,3,6,7].include?(n) ? ret += ('\\ ') : ret += ('  ') 
-          [1,3,5,7].include?(n) ? ret += ('/ ') : ret += ('  ') 
+          [2, 3, 6, 7].include?(n) ? ret += ('\\ ') : ret += ('  ')
+          [1, 3, 5, 7].include?(n) ? ret += ('/ ') : ret += ('  ')
         end
-        ret += "\n" 
-        counter+=1 
-        indentation+=2
+        ret += "\n"
+        counter += 1
+        indentation += 2
       end
     end
     ret += ' ' * 15 + 'a b c d e f g h i j k l m n o' + "\n"
@@ -50,31 +50,41 @@ class Board
   end
 
   def self.prepare_pairs
-    @cell_pairs = Hash.new
+    @cell_pairs = {}
     start_letter = 'a'
     increment = 0
     checker = 0
     CELL_CODES.each_with_index do |element, row, col|
       if row <= 5
-        if checker != row then # I get here everytime it goes to another row
+        if checker != row # I get here everytime it goes to another row
           increment = -row + 5
           checker = row
         end
-        if(element == 7) then
-          @cell_pairs["#{row} #{col}"] = "#{row.to_s}#{start_letter.increment(increment)}"
+        if element == 7
+          @cell_pairs["#{row} #{col}"] = "#{row}#{start_letter.increment(increment)}"
           increment += 2
         end
       else
-        if checker != row then # I get here everytime it goes to another row
+        if checker != row # I get here everytime it goes to another row
           increment = row - 5
           checker = row
         end
-        if(element == 7) then
-          @cell_pairs["#{row} #{col}"] = "#{row.to_s}#{start_letter.increment(increment)}"
+        if element == 7
+          @cell_pairs["#{row} #{col}"] = "#{row}#{start_letter.increment(increment)}"
           increment += 2
         end
       end
     end
-    return @cell_pairs
+    @cell_pairs
+  end
+
+  def self.calc_destination(x, y, dir)
+    board_pairs = Board.prepare_pairs
+    return nil if MOVES[dir].nil?
+    ret_x = x + MOVES[dir][0]
+    ret_y = y + MOVES[dir][1]
+    ret = [ret_x, ret_y]
+    return nil unless board_pairs["#{ret_x} #{ret_y}"]
+    ret
   end
 end
